bec1e5a87b40be1c8243d4c8356681176870f2ee
Gabriele <115879371+Gabriele9102@users.noreply.github.com>
2022-11-07 19:42:00 +0100

    Update main.js

--- 07-11-22/main.js
+++ 07-11-22/main.js
@@ -7,13 +7,6 @@ fetch("https://jsonplaceholder.typicode.com/users")
 .then((data) => data.map((newObj) => console.log("Name: " + newObj.name, "\n", "Address: " + Object.values(newObj.address), "\n", "City: " + newObj.address.city )))
 .catch((err) => console.log("error:" + err))
 
-
-
-fetch("https://jsonplaceholder.typicode.com/users")
-.then((response) => response.json())
-.then((data) => console.log("data:", data))
-
-
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then((response) => response.json())
   .then((data2) => data2.map((todos) => console.log("ID:" + todos.id, "\n", "Title" + todos.title)))
