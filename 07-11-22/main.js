14ec2c3c89aa8ca491f09527b741621ec30bbdb5
Gabriele <chiaramontegabriele4@gmail.com>
2022-11-07 16:54:17 +0100

    js-fetch-data

---
+++ 07-11-22/main.js
@@ -0,0 +1,21 @@
+
+//---------------------Esecuzione esercizio------------------------
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response) => response.json())
+.then((data) => data.map((newObj) => console.log("Name: " + newObj.name, "\n", "Address: " + Object.values(newObj.address), "\n", "City: " + newObj.address.city )))
+.catch((err) => console.log("error:" + err))
+
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response) => response.json())
+.then((data) => console.log("data:", data))
+
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => response.json())
+  .then((data2) => data2.map((todos) => console.log("ID:" + todos.id, "\n", "Title" + todos.title)))
+  .catch((err) => console.log("error:" + err))
+
