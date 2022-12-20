ebfd741cdfc839cafd708ad90bf3de776575332a
Gabriele <115879371+Gabriele9102@users.noreply.github.com>
2022-12-20 17:24:11 +0100

    React first commit

---
+++ 20-12-22/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { Btn } from './Components/Button console';
+
+
+function App() {
+
+  const console = "Hello Console!"
+  return (
+    <div className='app'>
+      <h1>{console}</h1>
+     <Btn/>
+    </div>
+  )
+}
+
+export default App;
---
+++ 20-12-22/src/Components/Button console.jsx
@@ -0,0 +1,12 @@
+
+
+export function Btn(){
+   const print =() => console.log('Hello World!');
+  
+
+   return (
+       <button className="Btn" onClick={print}>Click me!</button>
+   )
+};
+
+
